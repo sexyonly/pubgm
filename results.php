f731424a4cb0b9eedfde846054f5cf192718acb3
sexyonly <102304540+sexyonly@users.noreply.github.com>
2022-04-09 21:48:47 +0700

    Add files via upload

---
+++ results.php
@@ -0,0 +1,9 @@
+<?php
+
+    /* File ini sudah tidak dipakai, gunakan email.php untuk merubah
+    email dan merubah nama results. */
+    
+    /* This file is deprecated, use email.php to change the
+    email and rename the name results. */
+
+?>
